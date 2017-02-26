539fc6e504d35cc9e563d8bd569390b85f1f4dd3
fudi <fudi.tbg@gmail.com>
2017-02-26 16:52:44 +0100

    modificated html tamplate to display data of the  Bus models

--- busreserv/views.py
+++ busreserv/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Bus
 
 def panel(request):
-    return render(request, 'busreserv/panel.html', {})
+    buses = Bus.objects.all()
+    return render(request, 'busreserv/panel.html', {'buses' : buses})
