274024ec3774feef870fbd387c8c5ef26009aac7
fudi <fudi.tbg@gmail.com>
2017-02-27 15:00:02 +0100

    function added: editing bus

--- busreserv/views.py
+++ busreserv/views.py
@@ -8,16 +8,27 @@ def panel_view(request):
 
 def edit_vehicle_view(request, pk):
     vehicle = get_object_or_404(Bus, pk=pk)
-    return render(request, 'busreserv/edit_vehicle.html', {'vehicle':vehicle}) 
+    if request.method == "POST":
+        form_var = BusForm(request.POST, instance=vehicle)
+        if form_var.is_valid():
+            newBus = form_var.save(commit=False)           
+            newBus.save()                               
+            buses = Bus.objects.all()           #to previos site in broswer
+            return render(request, 'busreserv/panel.html', {'buses' : buses})
+    else:
+        form_var = BusForm(instance=vehicle)
+    return render(request, 'busreserv/new_vehicle.html', {'formBus': form_var})
 
 def new_vehicle_view(request):
     if request.method == "POST":
-        form_var = BusForm(request.POST)
+        form_var = BusForm(request.POST)        
         if form_var.is_valid():
-            newBus = form_var.save()
+            newBus = form_var.save()            #create nawBus 
             newBus.save()                       #save to database
             buses = Bus.objects.all()           #to previos site in broswer
             return render(request, 'busreserv/panel.html', {'buses' : buses})
     else:
         form_var = BusForm()
     return render(request, 'busreserv/new_vehicle.html', {'formBus': form_var})
+
+
