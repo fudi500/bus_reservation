6ba5bba077a1afd30fe96b7c70b6ea15f3be0b80
fudi500 <fudi.tbg@gmail.com>
2017-03-01 20:03:10 +0100

    database repaired

--- busreserv/migrations/0001_initial.py
+++ busreserv/migrations/0001_initial.py
@@ -2,27 +2,24 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Bus',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('description', models.CharField(max_length=200)),
                 ('brand', models.CharField(max_length=150)),
                 ('plate_nr', models.CharField(max_length=10)),
                 ('people_capacity', models.IntegerField()),
-                ('price_per_km', models.IntegerField()),
+                ('price_per_km', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('available_for_cutomers', models.BooleanField(default=True)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
