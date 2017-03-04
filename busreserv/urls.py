0af238b485c18a08c998da7218120cf74b214c63
fudi500 <fudi.tbg@gmail.com>
2017-03-04 22:50:10 +0100

    trello TO DO done

--- busreserv/urls.py
+++ busreserv/urls.py
@@ -3,15 +3,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.client_panel_view, name='client_panel_name'),
-    url(r'^panel/$', views.panel_view, name='panel'),
-    url(r'^panel/edit_vehicle/(?P<pk>[0-9]+)/$', views.edit_vehicle_view, name='edit_vehicle'),
-    url(r'^panel/new/$', views.new_vehicle_view, name='new_vehicle_name'),
-    url('^panel/(?P<pk>\d+)/delete/$', views.delete_vehicle_view, name='delete_vehicle_name'),
+
 
     url(r'^newreservation/(?P<pk>[0-9]+)/$', views.reservation_view, name='newreservation_name'),
     url(r'^newreservation/details(?P<pk>[0-9]+)/$', views.reservation_view, name='details'),
 
-    url(r'^panel/driver/$', views.new_driver_view, name='driver'),
-    url(r'^panel/edit_driver/(?P<pk>[0-9]+)/$', views.edit_driver_view, name='edit_driver'),
-    url(r'^panel/driver/deletedriver/(?P<pk>[0-9]+)/$', views.delete_driver_view, name='delete_driver'),
+    url(r'^panel/$', views.panel_view, name='panel'),
+
+    url(r'^panel/bus/edit/(?P<pk>[0-9]+)/$', views.edit_vehicle_view, name='edit_vehicle'),
+    url(r'^panel/bus/details/(?P<pk>[0-9]+)/$', views.vehicle_details_view, name='vehicle_details'),
+    url(r'^panel/bus/new/$', views.new_vehicle_view, name='new_vehicle_name'),
+    url('^panel/delete/(?P<pk>\d+)/$', views.delete_vehicle_view, name='delete_vehicle_name'),
+
+
+    url(r'^panel/driver/new$', views.new_driver_view, name='driver'),
+    url(r'^panel/driver/edit/(?P<pk>[0-9]+)/$', views.edit_driver_view, name='edit_driver'),
+    url(r'^panel/driver/delete/(?P<pk>[0-9]+)/$', views.delete_driver_view, name='delete_driver'),
 ]
--- busreserv/views.py
+++ busreserv/views.py
@@ -34,7 +34,9 @@ def new_driver_view(request):
             return HttpResponseRedirect("/panel")
     else:
         driverForm = DriverForm()
-    return render(request, 'busreserv/driver.html', {'driverForm':driverForm})
+    return render(request, 'busreserv/driver.html', {
+        'driverForm':driverForm
+    })
 
 def edit_driver_view(request, pk):
     driver = get_object_or_404(Driver, pk=pk)
@@ -45,11 +47,13 @@ def edit_driver_view(request, pk):
             return HttpResponseRedirect("/panel")
     else:
         driverForm = DriverForm(instance=driver)
-    return render(request, 'busreserv/driver.html', {'driverForm':driverForm})
+    return render(request, 'busreserv/driver.html', {
+        'driverForm':driverForm
+    })
 
 def delete_driver_view(request, pk):
-    vehicle = get_object_or_404(Bus, pk=pk)
-    vehicle.delete()
+    driver = get_object_or_404(Driver, pk=pk)
+    driver.delete()
     return HttpResponseRedirect("/panel")
 
 def edit_vehicle_view(request, pk):
@@ -57,10 +61,11 @@ def edit_vehicle_view(request, pk):
     if request.method == "POST":
         form_var = BusForm(request.POST, instance=vehicle)
         if form_var.is_valid():
-            newBus = form_var.save(commit=False)
-            newBus.save()
-            buses = Bus.objects.all()           #to previos site in broswer
-            return render(request, 'busreserv/panel.html', {'buses' : buses})
+            editedBus = form_var.save(commit=False)
+            editedBus.save()
+            return render(request, 'busreserv/bus_details.html', {
+                'Bus': editedBus
+            })
     else:
         form_var = BusForm(instance=vehicle)
     return render(request, 'busreserv/new_vehicle.html', {'formBus': form_var})
@@ -71,22 +76,31 @@ def new_vehicle_view(request):
         if form_var.is_valid():
             newBus = form_var.save()            #create nawBus
             newBus.save()                       #save to database
-            buses = Bus.objects.all()           #to previos site in broswer
-            return render(request, 'busreserv/panel.html', {'buses' : buses})
+            return render(request, 'busreserv/bus_details.html', {
+                'Bus': newBus
+            })
     else:
         form_var = BusForm()
-    return render(request, 'busreserv/new_vehicle.html', {'formBus': form_var})
+    return render(request, 'busreserv/new_vehicle.html', {
+        'formBus': form_var
+    })
 
+def vehicle_details_view(request, pk):
+    bus = get_object_or_404(Bus, pk=pk)
+    return render(request, 'busreserv/bus_details.html', {
+        'Bus': bus
+    })
 
 def delete_vehicle_view(request, pk):
     vehicle = get_object_or_404(Bus, pk=pk)
     vehicle.delete()
-    buses = Bus.objects.all()           #to previos site in broswer
-    return render(request, 'busreserv/panel.html', {'buses' : buses})
+    return HttpResponseRedirect("/panel")
 
 def client_panel_view(request):
     buses = Bus.objects.filter(available_for_cutomers=True)
-    return render(request, 'busreserv/client.html', {'buses' : buses})
+    return render(request, 'busreserv/client.html', {
+        'buses' : buses
+    })
 
 def reservation_view(request, pk):
     bus = get_object_or_404(Bus, pk=pk)
@@ -94,21 +108,15 @@ def reservation_view(request, pk):
     newreservation = Reservation()
     newreservation.reBusID = bus
     if request.method == "POST":
-
         form_var = ReservationForm(request.POST,instance=newreservation,initial={'reDate': datetime.datetime.now()})
         date = request.POST.get('reDate')
-#        if True:
-#            return HttpResponseRedirect('/This date is reserved for this bus, enter another dateor another bus./')
-
         if form_var.is_valid():
             newreservation = form_var.save(commit=False)
             if not request.POST.get('EndDate'):
                 newreservation.EndDate = request.POST.get('reDate') # if EndDate is none: EndDate = reDate
             else:
                 newreservation.EndDate = request.POST.get('EndDate')
-
             newreservation.save()
-
             price = bus.price_per_km * newreservation.km
 
             return render(request, 'busreserv/details.html', {
